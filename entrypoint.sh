eb82768f9a2bdc933444390c36051569a2a6ab06
Venura Athukorala <vathukorala@gmail.com>
2020-08-16 20:54:43 +1000

    input enhancements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,29 @@
 
 # Ensure the workflow fails on error
 set -e
-sh arm-ttk/Test-AzTemplate.sh -TemplatePath $1
+
+#_template_path=$1
+#_template_file=$2
+#_test=$3
+#_skip=$4
+
+_args = "-TemplatePath $1"
+
+if [$2]
+    then
+        $_args = "$_args -Test $2"
+fi
+
+if [$3]
+    then
+        $_args = "$_args -Test $3"
+    elif
+        if [$4]
+        then
+            $_args = "$_args -Skip $4"
+        fi
+fi
+
+sh arm-ttk/Test-AzTemplate.sh $_args
+
+
