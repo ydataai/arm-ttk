6169a68bb14e835eacbfc2bfc21c37f5ac6a0c9a
Venura Athukorala <venura9@users.noreply.github.com>
2020-08-17 21:43:00 +1000

    updated detailed comments with reference links

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Ensure the workflow fails on error
 set -e
 
+# List of args
 # -TemplatePath => $1
 # -File => $2
 # -Test => $3
@@ -29,9 +30,10 @@ echo "Running: sh arm-ttk/Test-AzTemplate.sh $_args"
 results=$(sh arm-ttk/Test-AzTemplate.sh $_args)
 
 echo "Results: $results"
-
+# https://github.com/actions/create-release/issues/25
 results="${results//'%'/'%25'}"
 results="${results//$'\n'/'%0A'}"
 results="${results//$'\r'/'%0D'}"
 
+#this sets the output
 echo "::set-output name=results::'$results'"
